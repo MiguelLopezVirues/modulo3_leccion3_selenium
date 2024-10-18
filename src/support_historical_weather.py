679810564f0489acb02d1a1ea334f1686a036f02
MiguelLopezVirues <miguellv96@gmail.com>
2024-10-18 09:00:44 +0200

    CORRECCION sobreescritura otro git

--- src/support_historical_weather.py
+++ src/support_historical_weather.py
@@ -34,85 +34,123 @@ import datetime
 def random_cities(n_cities, cities_df):
 
     randoms = cities_df.sample(n=n_cities)
+
+    city_names = randoms["city"]
+
+    city_codes = get_city_code_plural(city_names)
+
+    cities_df.loc[randoms.index,"code_wunder"] = pd.Series(city_codes)
+
+    cities_df.to_csv("../data/cities_coordinates_codes.csv")
+
+    return city_codes
+
+
+def get_city_code_plural(city_names):
+
     code_list = list()
-    for idx,random_city in randoms.iterrows():
-        if random_city["code_wunder"] == "":
-            random_city["code_wunder"] = get_city_code(random_city["city"])
-            cities_df.loc[cities_df["city"] == random_city["city"] ,"code_wunder"] = random_city["code_wunder"]
-
-        code_list.append(random_city["code_wunder"])
-    
-    return code_list
-
- 
-        
-def get_city_code(city_name):
-    link = f"https://www.wunderground.com/weather/es/{city_name}"
 
     driver = webdriver.Chrome()
-    driver.get(link)
 
-    driver.maximize_window()
+    first_link = f"https://www.wunderground.com/"
+
+    driver.get(first_link)
 
     handle_cookies(driver)
 
+    for city_name in city_names:
+
+        sleep(2)
+        link = f"https://www.wunderground.com/weather/es/{city_name}"
+
+        driver.get(link)
+
+        city_code = get_city_code(driver)
+
+        code_list.append(city_code)
+
+    return code_list
+        
+
+
+def get_city_code(driver, url=None, city_name=None):
+
     #display table on the page
-    sleep(5)
+    sleep(3)
     try:
         code_link = driver.find_element("css selector","#inner-content > div.region-content-top > lib-city-header > div:nth-child(1) > div > div > a.station-name").get_attribute("href")
+
         driver.get(code_link)
+
     except:
         print("Url to capture the city_code not well got.")
-        
+
     sleep(3)
+
     code = driver.find_element("css selector", "#inner-content > div.region-content-top > app-dashboard-header > div.dashboard__header.small-12.ng-star-inserted > div > div.heading > h1")
     code = code.text.split(" - ")[-1]
     return code
 
 def handle_cookies(driver):
+
+    driver.maximize_window()
+
     iframe = WebDriverWait(driver,15).until(EC.presence_of_element_located(('xpath','//*[@id="sp_message_iframe_1165301"]')))
+
     driver.switch_to.frame(iframe)
     try:
         driver.implicitly_wait(5)
         accept_button = driver.find_element("css selector","#notice > div.message-component.message-row.cta-buttons-container > div.message-component.message-column.cta-button-column.reject-column > button")
         accept_button.click()
     except:
-        print("Cookies were not correctly handled.")
+        print("Cookies were not correctly handled or the pop up was not there.")
     
     driver.switch_to.default_content()
 
     
-def get_city_table(city_code, year_number, month_number):
+def get_city_table_df(city_code, year_number, month_number,driver=False):
+    
     try:
+
         if int(month_number) < 1 and int(month_number) > 12:
             raise ValueError()
+        
         elif int(year_number) < 2020 and int(year_number) > datetime.now().year:
             raise ValueError()
+        
     except:
         print("You entered wrong values for year or month. Please enter numeric values. The first available year is 2020.")
 
     table_link = f"https://www.wunderground.com/dashboard/pws/{city_code}/table/{year_number}-{month_number}-01/{year_number}-{month_number}-31/monthly"
+    print(table_link)
 
     try:
-        driver = webdriver.Chrome()
-        driver.get(table_link)
+        if not driver:
+            print("No driver")
+            driver = webdriver.Chrome()
+            driver.get(table_link)
+            driver.maximize_window()
+            handle_cookies(driver)
+
+        else:
+            print("Reusing driver")
+            driver.get(table_link)
+
     except:
         print("The city entered was not found.")
 
-    driver.maximize_window()
 
-    handle_cookies(driver)
-    
-    driver.execute_script('window.scrollBy(0, 2000)')
-
-    sleep(4)
-
-    return scrape_table(driver, city_code)
+    return scrape_table(driver, city_code) , driver
 
 def scrape_table(driver, city_code):
     # get element with the table rows inside
-    rows = driver.find_element("css selector", "#main-page-content > div > div > div > lib-history > div.history-tabs > lib-history-table > div > div").text.split("\n")
+    try:
+        driver.implicitly_wait(10)
+        rows = driver.find_element("css selector", "#main-page-content > div > div > div > lib-history > div.history-tabs > lib-history-table > div > div").text.split("\n")
 
+    except:
+        print("There has been a problem getting the element.")
+    
     # remove special characters and split into cells
     df_table = pd.Series([row.replace("°F","").replace("%","").replace("mph","").replace("in","") for row in rows])
     df_table = df_table.str.split(expand=True)
@@ -126,8 +164,33 @@ def scrape_table(driver, city_code):
     
     # add city code
     df_table["city_code"] = city_code
+
+    print(df_table)
     
     return df_table
     
 
+def multi_scrape_table(n_cities, cities_df, n_months=1, start_year=2024, start_month=1):
+
+    city_codes = random_cities(n_cities, cities_df)
+
+    df_final = pd.DataFrame()
+
+    driver = None
+
+    for month in range(start_month, start_month + n_months):
+        for city_code in city_codes:
+
+            try:
+                df, driver = get_city_table_df(city_code,start_year,month, driver)
+                df_final = pd.concat([df_final,df],axis=0)
+
+            except:
+                print(f"There was a problem scraping the citiy code {city_code} for the month {month}.")
+        
+            df_final.to_csv("../data/historical_weather_saved.csv")
+    
+    driver.close()
+    return df_final
+        
 
